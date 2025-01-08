db7c6d804b8a363b58b76fe107a7b2c07471ea0d
harunonekohime <jessicaleeschulz@gmail.com>
2025-01-08 14:38:26 +0100

    feat: changed build folder for github public pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
